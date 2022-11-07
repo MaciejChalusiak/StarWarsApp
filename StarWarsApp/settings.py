8cf75febd334395ee3c1eef5ca843195e929a17f
Maciej Chalusiak <maciejchalusiak@gmail.com>
2022-11-07 11:54:11 +0100

    Requests have been optimized, added templates, changed urls, added new view to download dataset

--- StarWarsApp/settings.py
+++ StarWarsApp/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'StarWarsApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(SETTINGS_PATH, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- people/views.py
+++ people/views.py
@@ -1,17 +1,44 @@
-from django.http import JsonResponse, HttpResponse
+from django.http import HttpResponse, QueryDict
 import requests
 from people.models import DataSet, Person
 from datetime import datetime
 from people.serializers import PersonSerializer
 import logging
+from django.shortcuts import render
+import csv
+
+
+def people_home_page(request):
+    return render(request, "home.html")
 
 
 def show_dataset(request):
-    return HttpResponse(content=DataSet.objects.values())
+    dataset_list = list(DataSet.objects.values())
+    return render(request, "show_dataset.html", context={"dataset_list": dataset_list})
 
 
 def show_dataset_details(request, data_set_id):
-    return HttpResponse(content=Person.objects.filter(data_set=data_set_id).values())
+    person_list = list(Person.objects.filter(data_set=data_set_id).values())
+    query = QueryDict(request.META['QUERY_STRING'])
+    if limit := query.get('limit'):
+        person_list = person_list[0:int(limit)]
+    else:
+        limit = None
+    return render(request, "show_dataset_details.html", context={"person_list": person_list, "data_set_id": data_set_id, "limit": limit})
+
+
+def download_dataset(request, data_set_id):
+    response = HttpResponse(
+        content_type='text/csv',
+        headers={'Content-Disposition': f'attachment; filename=data_set_{data_set_id}.csv'},
+    )
+
+    writer = csv.writer(response)
+    person_list = list(Person.objects.filter(data_set=data_set_id).values())
+    writer.writerow(person_list[0].keys())
+    for person in person_list:
+        writer.writerow(list(person.values()))
+    return response
 
 
 def create_dataset(request):
@@ -19,12 +46,19 @@ def create_dataset(request):
 
     page = 1
     is_next_page = True
+    homeworld_dictionary = {}
     while is_next_page:
         response = requests.get(f"https://swapi.dev/api/people/?page={page}")
         for person in response.json()["results"]:
-            if url := person.get("homeworld"):
-                homeworld_response = requests.get(url)
-                person["homeworld"] = homeworld_response.json()["name"]
+
+            if homeworld_url := person.get("homeworld"):
+                if homweworld_name := homeworld_dictionary.get(homeworld_url):
+                    person["homeworld"] = homweworld_name
+                else:
+                    homeworld_response = requests.get(homeworld_url)
+                    homweworld_name = homeworld_response.json()["name"]
+                    person["homeworld"] = homweworld_name
+                    homeworld_dictionary[homeworld_url] = homweworld_name
 
             if edited := person.get("edited"):
                 person["date"] = edited.split("T")[0]
@@ -42,4 +76,7 @@ def create_dataset(request):
         page += 1
         is_next_page = response.json()["next"]
 
-    return HttpResponse(status=200, content="Data set is correctly created")
+    return HttpResponse(
+        status=200,
+        content=f"Data set is correctly created. id: '{data_set.id}', name: '{data_set.name}'"
+    )
